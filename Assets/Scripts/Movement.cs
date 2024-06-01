9fd37b0427fd9eca8d55b8459707f67e851c75b1
anurupkoli <anurupkoli01@gmail.com>
2024-06-01 11:34:32 +0530

    Added Sound to our Rocketee

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -5,12 +5,14 @@ using UnityEngine;
 public class Movement : MonoBehaviour
 {
     Rigidbody rb;
+    AudioSource[] audioSources;
     [SerializeField] float mainThrust = 1000f;
     [SerializeField] float rotationThrust = 200f;
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        audioSources = GetComponents<AudioSource>();
     }
 
     // Update is called once per frame
@@ -20,18 +22,37 @@ public class Movement : MonoBehaviour
         RocketeeRotate();
     }
 
-    void RocketeeThrust(){
-        if(Input.GetKey(KeyCode.Space)){
+    void RocketeeThrust()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            if (!audioSources[0].isPlaying)
+            {
+                audioSources[1].Stop();
+                audioSources[0].Play();
+            }
             rb.AddRelativeForce(Vector3.up * mainThrust * Time.deltaTime);
+        }
+        else
+        {
+
+            audioSources[0].Stop();
+            if (!audioSources[1].isPlaying)
+            {
+                audioSources[1].Play();
+            }
         }
     }
 
-    void RocketeeRotate(){
+    void RocketeeRotate()
+    {
         rb.freezeRotation = true; //freezing game's physics rotation so that I can maually rotate rocketee
-        if(Input.GetKey(KeyCode.A)){
+        if (Input.GetKey(KeyCode.A))
+        {
             transform.Rotate(Vector3.forward * rotationThrust * Time.deltaTime);
         }
-        else if(Input.GetKey(KeyCode.D)){
+        else if (Input.GetKey(KeyCode.D))
+        {
             transform.Rotate(-Vector3.forward * rotationThrust * Time.deltaTime);
         }
         rb.freezeRotation = false;//unfreezing game's physics rotation
