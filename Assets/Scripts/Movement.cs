f7492515f429f6ae993f1f32fa5e0c4a81cd1594
anurupkoli <anurupkoli01@gmail.com>
2024-06-02 15:40:47 +0530

    adding particle system

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -11,6 +11,8 @@ public class Movement : MonoBehaviour
     [SerializeField] AudioClip rocekteeThrust;
     [SerializeField] AudioClip rocketeeEngine;
     [SerializeField] ParticleSystem engineParticles;
+    [SerializeField] ParticleSystem leftThrustParticles;
+    [SerializeField] ParticleSystem rightThrustParticles;
     // Start is called before the first frame update
     void Start()
     {
@@ -29,9 +31,11 @@ public class Movement : MonoBehaviour
     {
         if (Input.GetKey(KeyCode.Space))
         {
-            engineParticles.Play();
             if (!audioSources[0].isPlaying)
             {
+                if(!engineParticles.isPlaying){
+                    engineParticles.Play();
+                }
                 audioSources[1].Stop();
                 audioSources[0].PlayOneShot(rocekteeThrust);
             }
@@ -39,6 +43,7 @@ public class Movement : MonoBehaviour
         }
         else
         {
+            engineParticles.Stop();
             audioSources[0].Stop();
             if (!audioSources[1].isPlaying)
             {
@@ -53,11 +58,23 @@ public class Movement : MonoBehaviour
         rb.freezeRotation = true; //freezing game's physics rotation so that I can maually rotate rocketee
         if (Input.GetKey(KeyCode.A))
         {
+            leftThrustParticles.Stop();
+            if(!rightThrustParticles.isPlaying){
+                rightThrustParticles.Play();
+            }
             transform.Rotate(Vector3.forward * rotationThrust * Time.deltaTime);
         }
         else if (Input.GetKey(KeyCode.D))
         {
+            rightThrustParticles.Stop();
+            if(!leftThrustParticles.isPlaying){
+                leftThrustParticles.Play();
+            }
             transform.Rotate(-Vector3.forward * rotationThrust * Time.deltaTime);
+        }
+        else{
+            leftThrustParticles.Stop();
+            rightThrustParticles.Stop();
         }
         rb.freezeRotation = false;//unfreezing game's physics rotation
     }
